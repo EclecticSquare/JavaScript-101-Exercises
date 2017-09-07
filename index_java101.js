712cc8cb93c0c9ac08edea192b8df8b0048810f2
Tina Bryson <tinabryson@tinas-mbp.attlocal.net>
2017-09-06 21:23:51 -0400

    latest version

--- index_java101.js
+++ index_java101.js
@@ -1,4 +1,5 @@
 // STEP ONE
+//returns hello plus the name as a string
 // function hello(name) {
 //   return "hello " + name;
 // };
@@ -7,6 +8,7 @@
 
 
 //STEP TWO
+//returns hello world if no name is given
 // function hello(name) {
 //   if (name){
 //   return "hello " + name;
@@ -19,3 +21,25 @@
 // console.log(hello('joe'));
 
 //STEP THREE
+// creates a phrase about the named person's favorite subject
+// function madLib(name, subject) {
+//   return name + "'s favorite subject in school is " + subject;
+// };
+//
+// console.log(madLib("bob", "science"));
+
+//STEP FOUR
+//calculates tip based on rating of good, fair or bad
+// function tipAmount (rating, num) {
+//   if (rating === "good") {
+//   return num * 0.20;
+//   }
+//   else if (rating === "fair") {
+//     return num * 0.15;
+//   }
+//   else if ( rating === "bad") {
+//     return num * 0.10;
+//   };
+// };
+//
+// console.log(tipAmount("bad", 100.10));
