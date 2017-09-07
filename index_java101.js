bf38f6abc3ec90c628e2b33b2928f82fa44fa9e5
Tina Bryson <tinabryson@tinas-mbp.attlocal.net>
2017-09-06 20:18:52 -0400

    updated

---
+++ index_java101.js
@@ -0,0 +1,21 @@
+// STEP ONE
+// function hello(name) {
+//   return "hello " + name;
+// };
+//
+// console.log(hello("bob"));
+
+
+//STEP TWO
+// function hello(name) {
+//   if (name){
+//   return "hello " + name;
+//   }
+//   else {
+//   return "hello world";
+//   }
+// };
+//
+// console.log(hello('joe'));
+
+//STEP THREE
